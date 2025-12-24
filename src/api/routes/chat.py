16d71d397319693d158868f565e578f8769159f2
nikizhi <nikita0142007@gmail.com>
2025-12-24 21:36:34 +0300

    Update chat.py

--- src/api/routes/chat.py
+++ src/api/routes/chat.py
@@ -6,7 +6,7 @@ from src.models.message import MessageCreate, MessageOut
 import src.repositories.sessions as sessions_repo
 import src.repositories.messages as messages_repo
 from src.services import bot
-
+from loguru import logger
 
 router = APIRouter(tags=["Chat"])
 
@@ -14,6 +14,7 @@ router = APIRouter(tags=["Chat"])
 @router.post("/chat/session")
 async def create_chat_session(current_user: CurrentUser, db_session: SessionDep):
     session = await sessions_repo.create_session(db_session, current_user.id)
+    logger.info(f'Сессия пользователя {current_user.username} успешно создана')
     return session
 
 
@@ -21,8 +22,10 @@ async def create_chat_session(current_user: CurrentUser, db_session: SessionDep)
 async def handle_message(current_user: CurrentUser, session: SessionDep, message_create: MessageCreate):
     check_session = await sessions_repo.get_session(session, message_create.session_id)
     if not check_session:
+        logger.warning(f'{current_user.username} попытался отправить сообщение в несуществующую сессию')
         raise HTTPException(status_code=404, detail="Сессия не найдена")
     if check_session.user_id != current_user.id:
+        logger.warning(f'{current_user.username} попытался сообщение в чужую сессию')
         raise HTTPException(status_code=403, detail="Доступ запрещен")
 
     await messages_repo.save_message(session, message_create)
@@ -32,6 +35,7 @@ async def handle_message(current_user: CurrentUser, session: SessionDep, message
     bot_answer = bot.get_bot_answer(message_create.text)
     bot_message_create = MessageCreate(session_id=message_create.session_id, sender_type="bot", text=bot_answer)
     await messages_repo.save_message(session, bot_message_create)
+    logger.info(f'Сообщение {message_create.text} от {current_user.username} успешно обработано')
     await asyncio.sleep(1.5)
     return { "answer": bot_answer }
 
@@ -40,11 +44,14 @@ async def handle_message(current_user: CurrentUser, session: SessionDep, message
 async def get_messages_history(current_user: CurrentUser, session: SessionDep, session_id: str):
     check_session = await sessions_repo.get_session(session, session_id)
     if not check_session:
+        logger.warning(f'{current_user.username} попытался получить сообщения из несуществующей сессии')
         raise HTTPException(status_code=404, detail="Сессия не найдена")
     if check_session.user_id != current_user.id:
+        logger.warning(f'{current_user.username} попытался получить сообщения из чужой сессии')
         raise HTTPException(status_code=403, detail="Доступ запрещен")
 
     messages = await messages_repo.get_messages_by_session_id(session, session_id)
+    logger.info(f'Доступ к сессии {session_id} успешно получен')
     return messages
 
 
@@ -52,8 +59,11 @@ async def get_messages_history(current_user: CurrentUser, session: SessionDep, s
 async def delete_messages_history(current_user: CurrentUser, session: SessionDep, session_id: str):
     check_session = await sessions_repo.get_session(session, session_id)
     if not check_session:
+        logger.warning(f'{current_user.username} попытался удалить несуществующую сессию')
         raise HTTPException(status_code=404, detail="Сессия не найдена")
     if check_session.user_id != current_user.id:
+        logger.warning(f'{current_user.username} попытался удалить чужую сессию')
         raise HTTPException(status_code=403, detail="Доступ запрещен")
 
+    logger.info(f'Сессия {session_id} успешно удалена')
     await messages_repo.delete_messages_by_session_id(session, session_id)
